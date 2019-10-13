53431908b6f8d1540ddbc7a28b38e0a7298eb6c1
emmaa19 <Windsnap96!>
2019-10-12 21:11:05 -0700

    createAccount and login

---
+++ climateherofront/src/components/CreateAccount.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class CreateAccount extends Componet {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    componentDidMount() {
+
+    }
+    hand
+    fillInAccount = async () => {
+
+    }
+
+    render() {
+            <input type="string" value={{this.state.inputValue} onChange{this.handleChange}>
+    }
+}
